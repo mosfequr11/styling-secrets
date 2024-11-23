35a2d1de13a7f0594197c19370832da3f16cca9a
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-11-24 02:45:06 +0600

    add: banner section

---
+++ bangla-shop/assets/js/script.js
@@ -0,0 +1,11 @@
+console.log("Hellow Wrold");
+
+// Banner Slider Section
+
+var swiper = new Swiper(".banner-slide", {
+  direction: "vertical",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
